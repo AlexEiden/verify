46eb42d3a06e826788019819d11c6c09a4943567
William Goodall <wgoodall01@gmail.com>
2016-12-19 18:44:15 -0500

    Implement client-side hashing

--- client/app/components/fileSubmitter.jsx
+++ client/app/components/fileSubmitter.jsx
@@ -1,20 +1,41 @@
 import React from "react";
 import Dropzone from "react-dropzone";
+import CryptoJS from "crypto-js";
 
 export default class extends React.Component{
     onDrop(acceptedFiles){
-        console.log("Accepted:");
-        console.dir(acceptedFiles);
+        var file = acceptedFiles[0];
+        
         this.setState({
-            file:acceptedFiles[0]
-        })
+            "file":file
+        });
+
+        var reader = new FileReader();
+
+        reader.addEventListener(
+            'load',
+            function () {
+                console.log("Hash:");
+                console.dir(file);
+                var wordArray = CryptoJS.lib.WordArray.create(this.result);
+                var hash = CryptoJS.SHA256(wordArray);
+                var hashHex = hash.toString(CryptoJS.enc.Hex);
+                console.log(hashHex);
+            }
+        );
+        reader.readAsArrayBuffer(file);
+    }
+
+    constructor(props){
+        super(props);
+        this.state = {file:{name:"<none>"}};
     }
 
     render(){
         return (
             <div>
                 <Dropzone onDrop={this.onDrop} multiple={false}>
-                    <div>This is a Dropzone!</div>
+                    <div>Name: {this.state.file.name}</div>
                 </Dropzone>
             </div>
         )
