5e93cbe7b5b57dea92a1144047ac4c2188dc1064
William Goodall <wgoodall01@gmail.com>
2016-12-19 18:44:07 -0500

    Remove test scss from mainPage

--- client/app/pages/mainPage.jsx
+++ client/app/pages/mainPage.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router';
 import FileSubmitter from "components/fileSubmitter.jsx";
-import "pages/mainPage.scss";
 
 export class MainPage extends React.Component{
     render(){
