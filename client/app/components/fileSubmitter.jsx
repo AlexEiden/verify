25e8551ba2a897aefd557ef2293a06b5b20ece21
William Goodall <wgoodall01@gmail.com>
2017-01-11 22:51:25 -0500

    File submitter shadow type thing

--- client/app/components/fileSubmitter.jsx
+++ client/app/components/fileSubmitter.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Dropzone from "react-dropzone";
 import CryptoJS from "crypto-js";
+import "components/fileSubmitter.scss";
 
 export default class extends React.Component{
     onDrop(acceptedFiles){
@@ -14,28 +15,34 @@ export default class extends React.Component{
 
         reader.addEventListener(
             'load',
-            function () {
+             ()=> {
                 console.log("Hash:");
                 console.dir(file);
                 var wordArray = CryptoJS.lib.WordArray.create(this.result);
                 var hash = CryptoJS.SHA256(wordArray);
                 var hashHex = hash.toString(CryptoJS.enc.Hex);
                 console.log(hashHex);
-            }
+				this.setState({hash:hashHex});
+				this.forceUpdate();
+			}
         );
         reader.readAsArrayBuffer(file);
     }
 
     constructor(props){
         super(props);
-        this.state = {file:{name:"<none>"}};
+        this.state = {
+			file: {name:"<none>"},
+			hash: "<no hash yet>"
+		};
     }
 
     render(){
         return (
             <div>
-                <Dropzone onDrop={this.onDrop} multiple={false}>
+                <Dropzone className="dropzone" onDrop={this.onDrop} multiple={false}>
                     <div>Name: {this.state.file.name}</div>
+					<div>Hash: {this.state.hash}</div>
                 </Dropzone>
             </div>
         )
