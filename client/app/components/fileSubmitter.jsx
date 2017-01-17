fb35b567bb76c9fdc7e4a473d1e69481d4c0a5da
William Goodall <wgoodall01@gmail.com>
2017-01-17 17:46:50 -0500

    FileSubmitter improvements

--- client/app/components/fileSubmitter.jsx
+++ client/app/components/fileSubmitter.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 import Dropzone from "react-dropzone";
 import CryptoJS from "crypto-js";
+import ServerClock from "components/serverClock.jsx";
 import "components/fileSubmitter.scss";
+import dropImg from "resources/drop.svg";
+
+var AttributePair = (props) => (
+	<div className="ap">
+		<b className="ap-attribute">{props.attr}</b>
+		<div className="ap-value">{props.value}</div>
+	</div>
+)
 
 export default class extends React.Component{
     onDrop(acceptedFiles){
@@ -21,6 +30,7 @@ export default class extends React.Component{
                 var hash = CryptoJS.SHA256(wordArray);
                 var hashHex = hash.toString(CryptoJS.enc.Hex);
 				self.setState({hash:hashHex});
+				console.dir(file);
                 console.log(`Hash of ${file.name} is ${hashHex}`);
 			}
         );
@@ -31,8 +41,8 @@ export default class extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-			file: {name:"<none>"},
-			hash: "<no hash yet>"
+			file: undefined,
+			hash: undefined,
 		};
     }
 
@@ -40,9 +50,15 @@ export default class extends React.Component{
         return (
             <div>
                 <Dropzone className="dropzone" onDrop={(f) => this.onDrop(f)} multiple={false}>
-                    <div>Name: {this.state.file.name}</div>
-					<div>Hash: {this.state.hash}</div>
-                </Dropzone>
+				{ this.state.file ? (<div>
+						<div className="dropzone-filename">{this.state.file.name}</div>
+						<AttributePair attr="Last changed on" value={this.state.file.lastModifiedDate.toString()}/>
+						<AttributePair attr="Signature time" value={<ServerClock/>}/>
+					</div>)
+				:
+					<img src={dropImg} width="100px" height="100px"/>	
+				}
+				</Dropzone>
             </div>
         )
     }
